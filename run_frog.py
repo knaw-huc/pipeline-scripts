0473f98cb1ef7733774b7919d84559de9af10d49
LB <lhuygi@users.noreply.github.com>
2019-02-12 13:27:53 +0100

    Run Frog on text files to get NER in FoLiA files

---
+++ run_frog.py
@@ -0,0 +1,23 @@
+# Runs frog on all files in argv[1] to produce NER'd FoLiA-format output
+# in the dir argv[2]. A log is kept in frog.log.
+# This uses the LaMachine Docker image.
+
+import os
+from os.path import abspath
+import subprocess
+import sys
+
+inputdir, outputdir = map(abspath, sys.argv[1:])
+
+os.makedirs(outputdir, exist_ok=True)
+
+with open('frog.log', 'a') as log, open('/dev/null', 'w') as null:
+    subprocess.call(["docker", "run",
+                     # inputdir as /in, read-only volume
+                     "-v", "%s:/in:ro" % inputdir,
+                     # outputdir as /out, read-write volume
+                     "-v", "%s:/out" % outputdir,
+                     "proycon/lamachine", "frog",
+                     "--skip=mptcla",
+                     "--testdir=/in", "--xmldir=/out"],
+                    stderr=log, stdout=null)
